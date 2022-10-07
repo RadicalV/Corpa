e75dd5f2fb1e1c658ebd36141865df161dcd313c
Tautvydas <hawklion2000@gmail.com>
2022-10-07 15:09:11 +0300

    Setup

---
+++ server/src/constants/index.ts
@@ -0,0 +1,7 @@
+const ORIGIN = "*";
+const DATABASE_URL = "";
+const PORT = "3000";
+const NODE_ENV = process.env.NODE_ENV || "development";
+const JWT_SECRET = "secret";
+
+export { ORIGIN, DATABASE_URL, PORT, NODE_ENV, JWT_SECRET };
